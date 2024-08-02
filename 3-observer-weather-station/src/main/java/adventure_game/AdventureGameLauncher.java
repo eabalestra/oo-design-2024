60dbb260767b160c3920fde3ea4be195681c1c55
eabalestra <agusbalestra0@gmail.com>
2024-08-02 19:02:11 -0300

    observer: fix diagrams and clients

--- 3-observer-weather-station/src/main/java/adventure_game/AdventureGameLauncher.java
+++ 3-observer-weather-station/src/main/java/adventure_game/AdventureGameLauncher.java
@@ -8,13 +8,13 @@ import adventure_game.observer.FileLogger;
 import adventure_game.observer.StatisticsLogger;
 import adventure_game.weapon.Sword;
 
-public class Main {
+public class AdventureGameLauncher {
     public static void main(String[] args) {
         Character firstCharacter = new Knight();
         Character secondCharacter = new Archer();
         firstCharacter.setWeapon(new Sword());
-
         BattleArena battleArena = new BattleArena(firstCharacter, secondCharacter);
+
         battleArena.registerObserver(new ConsoleLogger());
         battleArena.registerObserver(new FileLogger("battle_log.txt"));
         battleArena.registerObserver(new StatisticsLogger());
